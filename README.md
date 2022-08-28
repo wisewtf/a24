3c9210cf0d3b89925c7a68d9aabe17b3567f8101
wisewtf <admin@wise.wtf>
2022-08-28 15:16:36 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# a24
-A simple Discord webhook bot that will warn when new A24 movies are added to their Upcoming Movies website page
+# A24
+
+A simple Discord webhook bot that will warn when new A24 movies are added to their Upcoming Movies website page.
+
+All it does is scrape their [Films](https://a24films.com/films) page. Parse the `h3` HTML tag that contains movie titles and write them into a file in the `current working directory` of the script.
+
+Then every time this runs it will parse the written file and find items that are not in it when A24 updates their website.
+
+After that it will reach out to the OMDB API and try to find info on the new items and the script will compose a Discord Embed with all the info and sends it using a webhook URL in a channel you chose.
+
+Currently it doesn't really 'handle' movies that aren't in the API. It passes so not to break the for loop. But I'm considering expanding it or at least warn of the change in the channel, so people can go look for themselves if they are interested.
+
+You must install `requests`, `BeautifulSoup`, `DiscordWebhook` and `DiscordEmbed` to use this script.
+
+And then you can use cron or systemd to run it on a timer.
+
+In a future update I will modify the script to read the OS ENV table so that I can define API KEYS and Webhook URL inside a systemd service unit as service variables and then complete it with a timer unit.
+
+Criticism appreciated, this is my first fully working Python code.
---
+++ a24.py
@@ -0,0 +1,75 @@
+import requests
+import re
+import os
+from bs4 import BeautifulSoup
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from datetime import date
+import json
+
+DIR = os.getcwd()
+R = re.compile(r'<[^>]+>') 
+URL = 'https://a24films.com/films/'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
+}
+YEAR = date.today().year
+API = 'FILL_THIS_WITH_OMDB_API'
+WEBHOOK = DiscordWebhook(url='FILL_THIS_WITH_DISCORD_WEBHOOK_URL', rate_limit_retry=True)
+
+films_page = requests.get(URL, headers=HEADERS)
+parser = BeautifulSoup(films_page.content, "html.parser")
+found_items = parser.find_all("h3")
+
+movies = []
+for item in found_items:
+    stripped_movie = re.sub(R, '', str(item))
+    movies.append(stripped_movie)
+
+movie_list = []
+with open(f'{DIR}/old_movies.json', 'r') as fp:
+    for line in fp:
+        x = line[:-1]
+        movie_list.append(x)
+
+new_movie = []
+for movie in movies:
+    if movie not in movie_list:
+        title = str(movie)
+        API_URL = f'http://www.omdbapi.com/?apikey={API}&t={title}&y={YEAR}'
+        omdb_data = requests.get(API_URL)
+        new_movie.append(omdb_data.text)
+
+with open(f'{DIR}/old_movies.json', 'w') as fp:
+    for title in movies:
+        fp.write("%s\n" % title)
+
+
+for film in new_movie:
+    y = json.loads(film)
+    response = y["Response"]
+    if response == "True":
+        title = y["Title"]
+        plot = y["Plot"]
+        poster = y["Poster"]
+        rating = y["Rated"]
+        release = y["Released"]
+        runtime = y["Runtime"]
+        director = y["Director"]
+        writer = y["Writer"]
+        genre = y["Genre"]
+        embed = DiscordEmbed(title=f'{title}', description=f'{plot}', color='ffffff')
+        embed.set_author(name='NEW A24 UPCOMING MOVIE', url='https://a24films.com/films', icon_url='https://i.imgur.com/Il4vJct.png')
+        embed.set_image(url=f'{poster}')
+        embed.set_thumbnail(url='https://i.imgur.com/Il4vJct.png')
+        embed.set_footer(text='A24 Upcoming Movies bot coded by WISE (POG)')
+        embed.set_timestamp()
+        embed.add_embed_field(name='Director', value=f'{director}')
+        embed.add_embed_field(name='Writer', value=f'{writer}')
+        embed.add_embed_field(name='Genre', value=f'{genre}')
+        embed.add_embed_field(name='Rating', value=f'{rating}')
+        embed.add_embed_field(name='Runtime', value=f'{runtime}')
+        embed.add_embed_field(name='Release Date', value=f'{release}')
+        WEBHOOK.add_embed(embed)
+        response = WEBHOOK.execute(remove_embeds=True)
+    else:
+        pass
